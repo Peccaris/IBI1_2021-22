88f02e86b093f0cc32f4d00ada454b0ba9a73b22
Peccaris <101113101+Peccaris@users.noreply.github.com>
2022-05-23 22:22:23 +0800

    correct practical5

--- Practical5/variable.py
+++ Practical5/variable.py
@@ -1,12 +1,28 @@
-<<<<<<< HEAD
-print('Hello Wolrd!')
-=======
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Mon Mar 14 20:50:24 2022
+#desrib which year has the greastest number of new COVID-19
+a = 19245301
+b = 4218520
+c = 271
+d = b-c
+e = a-b
+if d>e:
+    print("the rate of new case in 2020 is greater than in 2021")
+elif d<e:
+    print("the rate of new case in 2021 is greater than in 2020")
+else:
+    print("the rate of new case in 2020 is the same sa in 2021")
 
-@author: yiyi
-"""
+#example：W=X+Y
+if 3<5: # You can change the numbers
+  X = True
+else:
+  X = False
 
->>>>>>> 633c4229b9dc298c06606b4fbcbfd2a116763908
+if 9<15: # You can change the numbers
+  Y = True
+else:
+  Y = False
+
+W = X and Y
+print('W is', W)
+# W should be true only if both X and Y are True
+
