4dc81c1c9165560ccca8515aad44f3e1c33af749
Peccaris <101113101+Peccaris@users.noreply.github.com>
2022-05-24 10:08:56 +0800

    Delete Practical8 directory

--- Practical8/ fragment_counter.py
+++
@@ -1,5 +0,0 @@
-import re  #import regulation expression
-
-seq = 'ATGCAATCGACTACGATCAATCGAGGGCC' #creat a string variable
-y = len(re.findall("GAATTC",seq))+1 # find EcoRI cutting sequence and count the number of fragments into which the sequence would be but  
-print(' The	 total	number of fragments cut by EcoRI enzyme is: ',y) # print the result
