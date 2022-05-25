4b6b5b1ed63114ccdf168b2ecb2a8a43f3e0a70f
Peccaris <101113101+Peccaris@users.noreply.github.com>
2022-05-25 08:36:14 +0800

    correct practical 14

--- Practical 14/childnode number.py
+++ Practical 14/childnode number.py
@@ -1,6 +1,7 @@
 import xml.dom.minidom
 import matplotlib.pyplot as plt
 
+
 print("start")
 DOMTree = xml.dom.minidom.parse(
     "/Users/yiyi/Desktop/IBI1_2021-22/Practical 14/go_obo.xml")
@@ -10,31 +11,39 @@ terms = obo.getElementsByTagName("term")
 # get the total number of terms currently recorded in the Gene Ontology
 print('The total number of terms is', len(terms))
 
-# create a dictionary to store the terms and their childnode numbers
+# create dictionaries and lists to store the terms and their childnode numbers
 childnode_dictionary = {}
 childnode_number = []
 childnode_translation_number = []
+visit_dictionary = {}
 
-#define a function to help calculate the childnode number
+# define a function to help calculate th childnode numbers
 def getchild(term):
+    if term in visit_dictionary:
+        return 0
+    visit_dictionary[term] = 1
     childnum = 0
-    for child in childnode_dictionary[term]:
-        childnum += getchild(child)
+    if term in childnode_dictionary:
+        for child in childnode_dictionary[term]:
+                childnum += getchild(child)
     return childnum+1
 
+#read the whole text to build parent-child relationship
 
-# read the whole text to build parent-child relationship
 for term in terms:
     term_id = term.getElementsByTagName('id')[0].childNodes[0].data
-    childnode_dictionary[term_id] = []
-    for child in term.getElementsByTagName('is_a'):
-        childnode_dictionary[term_id].append(child.childNodes[0].data)
+    for parent in term.getElementsByTagName('is_a'):
+        parent_id = parent.childNodes[0].data
+        if parent_id not in childnode_dictionary:
+            childnode_dictionary[parent_id] = [term_id]
+        else:
+            childnode_dictionary[parent_id].append(term_id)
 print("done")
-
-# calculate the number of child for each term
+#calculate the number of child for each term
 for term in terms:
     term_id = term.getElementsByTagName('id')[0].childNodes[0].data
-    childnum = getchild(term.getElementsByTagName('id')[0].childNodes[0].data)
+    visit_dictionary.clear()
+    childnum = getchild(term_id)
     childnode_number.append(childnum-1)
     term_def = term.getElementsByTagName('def')
     term_def_str = term_def[0].getElementsByTagName(
@@ -45,7 +54,7 @@ for term in terms:
 
 # plot the first chart
 plt.boxplot(childnode_number,
-            flierprops={'marker': '.',
+            flierprops={'marker': '*',
                         'markerfacecolor': 'red', 'color': 'black'},
             medianprops={'linestyle': '--', 'color': 'orange'}
             )
@@ -58,18 +67,17 @@ plt.show()
 
 # plot a second chart
 plt.boxplot(childnode_translation_number,
-            flierprops={'marker': '.',
+            flierprops={'marker': '*',
                         'markerfacecolor': 'red', 'color': 'black'},
             medianprops={'linestyle': '--', 'color': 'orange'}
             )
 plt.xlabel = ("terms associated with 'translation'")  # set x label
-plt.ylabel("childnode number of terms associated with 'translation'",
+plt.ylabel("childnode number",
            fontsize=10)  # set y label
 # set a title for the boxplot
 plt.title("The Distribution of Childnodes Across Terms Associated with 'translation'")
 
 plt.show()
-# The boxplot was squashed because there was too much data and the maximum was too large
 
 # calculate the averge number
 average_childnode_number = sum(childnode_number)/len(childnode_number)
